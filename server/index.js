c9ad11eaec31dc6b011437a93aa1627fee2b88c8
NoamBoni <noamboni55@gmail.com>
2022-08-22 19:09:06 +0300

    added db connection and models

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const app = express();
 
 dotenv.config({ path: '.env' });
 
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('connected to mongo'))
+    .catch(err => console.log(err));
+
 const port = process.env.PORT;
 const url = process.env.SERVER_URL;
 
