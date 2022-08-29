7c17f83f6b95887c1f9a1d83db2dd44129caf230
NoamBoni <noamboni55@gmail.com>
2022-08-29 22:07:34 +0300

    added review model and finished products

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const Product = require('../models/productModel');
 const { sendRes } = require('../helpers/sendRes');
 const { makeFilterObject } = require('../helpers/makeFilterObject');
@@ -25,6 +27,49 @@ module.exports.getAllProducts = async (req, res) => {
     try {
         const products = await Product.find(filterObject);
         sendRes(res, products, 200);
+    } catch (err) {
+        sendRes(res, err, 400, true);
+    }
+};
+
+module.exports.getProductById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product) throw new Error('id does not exist');
+        sendRes(res, product, 200);
+    } catch (err) {
+        sendRes(res, err, 400, true);
+    }
+};
+
+module.exports.deleteProductById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.deleteOne({ _id: id });
+        sendRes(res, {}, 204);
+    } catch (err) {
+        sendRes(res, err, 400, true);
+    }
+};
+
+module.exports.checkValidId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        const error = { message: 'invalid id' };
+        sendRes(res, error, 400, true);
+        return;
+    }
+    next();
+};
+
+module.exports.updateProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        sendRes(res, product, 200);
     } catch (err) {
         sendRes(res, err, 400, true);
     }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -9,4 +9,11 @@ router
     .get(productController.getAllProducts)
     .post(productController.createNewProduct);
 
+router
+    .route('/:id')
+    .all(productController.checkValidId)
+    .get(productController.getProductById)
+    .delete(productController.deleteProductById)
+    .put(productController.updateProduct);
+
 module.exports = router;
